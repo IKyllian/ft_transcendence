bb436bf63d6583d3e155f35e2af8b7e50b762157
unknown <lbertran@student.42lyon.fr>
2022-09-12 11:57:29 +0200

    add user avatar uploading

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+import { Controller, Get, Post, Request, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Observable, of } from "rxjs";
 import { GetUser } from "src/auth/decorator/get-user.decorator";
 import { JwtGuard } from "src/auth/guard/jwt.guard";
 import { User } from "src/entities/user.entity";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from "uuid";
+import * as path from 'path';
+
+export const avatarStorage = {
+	storage: diskStorage({
+		destination: process.cwd() + '/uploads/avatars',
+		filename: (_req, file, cb) => {
+			const name: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+			const extension: string = path.parse(file.originalname).ext;
+
+			cb(null, `${name}${extension}`)
+		}
+	})
+}
 
 @Controller('users')
 export class UserController {
@@ -12,5 +27,14 @@ export class UserController {
 	@Get('me')
 	getMe(@GetUser() user: User) {
 		return user;
+	}
+
+	@UseGuards(JwtGuard)
+	@Post('upload')
+	@UseInterceptors(FileInterceptor('file', avatarStorage))
+	uploadFile(@UploadedFile() file, @Request() req) : Observable<Object> {
+		console.log(req);
+		console.log(file);
+		return of({imagePath: file.path})
 	}
 }
