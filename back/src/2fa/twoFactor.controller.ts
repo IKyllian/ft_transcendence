93155dd0c86dd89d90462731a9de6085bbdc3e3f
Loïc <lbertran@student.42lyon.fr>
2022-10-31 13:43:27 +0100

    set enabled anyway

--- back/src/2fa/twoFactor.controller.ts
+++ back/src/2fa/twoFactor.controller.ts
@@ -1,16 +1,18 @@
-import { Body, ClassSerializerInterceptor, Controller, Post, Res, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, Post, Res, UnauthorizedException, UseGuards, UseInterceptors } from "@nestjs/common";
 import { JwtGuard } from "src/auth/guard/jwt.guard";
 import { User } from "src/typeorm";
 import { GetUser } from "src/utils/decorators";
 import { TwoFactorService } from "./TwoFactor.service";
 import { Response } from 'express';
 import { EnableTwoFactorDto } from "./dto/enable2fa.dto";
+import { UserService } from "src/user/user.service";
 
 @Controller('2fa')
 @UseInterceptors(ClassSerializerInterceptor)
 export class TwoFactorController {
 	constructor(
-		private readonly twoFactorService: TwoFactorService
+		private readonly twoFactorService: TwoFactorService,
+		private readonly userService: UserService
 	){}
 
 	@Post('generate')
@@ -27,5 +29,9 @@ export class TwoFactorController {
 		console.log(user); // y'a pas le field two_factor_secret dans le User jsp pourquoi ca m'enerve
 		
 		const isValid = this.twoFactorService.verify(body.code, user);
+
+		if (!isValid)
+			throw new UnauthorizedException('Invalid 2FA code');
+		await this.userService.setTwoFactorEnabled(user, true);
 	}
 }
