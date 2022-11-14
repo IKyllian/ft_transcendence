ced0347d75289819fd1649aebcd60426e280bf6f
Gersho <zennoune.karim@outlook.fr>
2022-11-14 19:49:01 +0100

    removed game folder, added assets folder

--- game/pong_server/src/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventsGateway } from './events.gateway';
-import { LobbyFactory } from '../lobby/lobby.factory';
-
-
-@Module({
-	providers: [EventsGateway, LobbyFactory],
-})
-export class EventsModule {}
--- game/test_client/clean_before_push.sh
+++
@@ -1,5 +0,0 @@
-rm -rf dist/static/*.js
-rm -rf dist/*.ts
-rm -rf dist/*.js
-rm -rf dist/*.map
-rm -rf dist/*.tsbuildinfo
