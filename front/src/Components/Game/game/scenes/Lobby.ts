f0a8ad31d316429cbaeaf8683ad69c08bb7dba2a
Zennoune Karim <kzennoun@student.42lyon.fr>
2022-12-16 11:22:59 +0100

    added game close setter

--- front/src/Components/Game/game/scenes/Lobby.ts
+++ front/src/Components/Game/game/scenes/Lobby.ts
@@ -104,7 +104,7 @@ export default class Lobby extends Phaser.Scene
 
 	create ()
 	{
-		this.game.registry.get('setHasEnded')(true);
+		// this.game.registry.get('setHasEnded')(true);
 		this.cameras.main.setBackgroundColor("#415A77");
 
 		this.me = this.game.registry.get('players_data').player_type;
@@ -281,6 +281,7 @@ export default class Lobby extends Phaser.Scene
 							.setOrigin(0.5,0.5);
 
 		setTimeout(() => {
+			this.game.registry.get('setHasEnded')(true);
 			this.game.destroy(true, false);
 		}, 5000);
 	}
@@ -330,6 +331,7 @@ export default class Lobby extends Phaser.Scene
 			 .setOrigin(0.5,0.5);
 
 		setTimeout(() => {
+			this.game.registry.get('setHasEnded')(true);
 			this.game.destroy(true, false);
 		}, 5000);
 	}
@@ -347,6 +349,7 @@ export default class Lobby extends Phaser.Scene
 								.setOrigin(0.5,0.5);
 
 			setTimeout(() => {
+				this.game.registry.get('setHasEnded')(true);
 				this.game.destroy(true, false);
 			}, 5000);
 		}
