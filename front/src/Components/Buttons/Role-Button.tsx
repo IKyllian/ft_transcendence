dbc039a3f0546bf418c74839c7aa7020a2265372
IKyllian <kyllian.delporte@gmail.com>
2022-12-21 10:41:34 +0100

    added set as owner button

--- front/src/Components/Buttons/Role-Button.tsx
+++ front/src/Components/Buttons/Role-Button.tsx
@@ -1,11 +1,14 @@
 import { useContext, useState } from "react";
 import { SocketContext } from "../../App";
+import { unsetOwner } from "../../Redux/ChannelSlice";
+import { useAppDispatch, useAppSelector } from "../../Redux/Hooks";
 import { ChannelUser } from "../../Types/Chat-Types";
-import AlertValidation from "../Alert-Validation";
 
 function RoleButton(props: {sender: ChannelUser}) {
     const {sender} = props;
     const [showAlert, setShowAlert] = useState<boolean>(false);
+    const {loggedUserIsOwner} = useAppSelector(state => state.channel);
+    const dispatch = useAppDispatch();
 
     const closeAlert = () => {
         setShowAlert(false);
@@ -21,11 +24,22 @@ function RoleButton(props: {sender: ChannelUser}) {
         })
     }
 
+    const ownerClick = () => {
+        socket?.emit("ChangeRole", {
+            userId: sender.user.id,
+            chanId: sender.channelId,
+            role: "owner",
+        })
+        dispatch(unsetOwner());
+    }
+
     return (
         <>
-            {/* <AlertValidation closeFunction={closeAlert} validateFunction={handleClick} textAlert="Ceci est une alerte" /> */}
             <p onClick={() => handleClick()}> {sender.role === "clampin" ? "Set as administrator" : "Set as normal user"}  </p>
-            {/* <p onClick={() => handleClick()}> {sender.role === "clampin" ? "Set as Owner"}  </p> */}
+            {
+                loggedUserIsOwner && 
+                <p onClick={() => ownerClick()}> Set as Owner  </p>
+            }
         </>
     );
 }
