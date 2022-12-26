c23321a05880c85b8557f46046e3b6c561f592fe
IKyllian <kyllian.delporte@gmail.com>
2022-12-26 17:53:13 +0100

    remove custom tooltip for rank and just keep title attribute

--- front/src/Components/Display-Rank.tsx
+++ front/src/Components/Display-Rank.tsx
@@ -9,17 +9,17 @@ function DisplayRank(props: {elo: number}) {
     const { elo } = props;
 
     if (elo < 1000)
-        return ( <div className="rank-icon" data-tooltips-rank="Silver"> <img src={Silver} alt="Rank icone" /> </div>);
+        return ( <img title="Silver" src={Silver} alt="Rank icone" /> );
     else if (elo >= 1000 && elo < 1125)
-        return ( <div className="rank-icon" data-tooltips-rank="Champion"> <img src={Gold} alt="Rank icone" /> </div>);
+        return ( <img title="Gold" src={Gold} alt="Rank icone" /> );
     else if (elo >= 1125 && elo < 1250)
-        return ( <div className="rank-icon" data-tooltips-rank="Platine"> <img src={Platine} alt="Rank icone" /> </div>);
+        return ( <img title="Platine" src={Platine} alt="Rank icone" /> );
     else if (elo >= 1250 && elo < 1375)
-        return ( <div className="rank-icon" data-tooltips-rank="Diamond"> <img src={Diamond} alt="Rank icone" /> </div>);
+        return ( <img title="Diamond" src={Diamond} alt="Rank icone" /> );
     else if (elo >= 1375 && elo < 1500)
-        return ( <div className="rank-icon" data-tooltips-rank="Champion"> <img src={Champion} alt="Rank icone" /> </div>);
+        return ( <img title="Champion" src={Champion} alt="Rank icone" /> );
     else
-        return ( <div className="rank-icon" data-tooltips-rank="Legend"> <img src={Legend} alt="Rank icone" /> </div>);
+        return ( <img title="Legend" src={Legend} alt="Rank icone" /> );
 }
 
 export default DisplayRank;
