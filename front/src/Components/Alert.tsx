ac62ce06c6362bc93f86f4c24ebb9272b75ec8d2
IKyllian <kyllian.delporte@gmail.com>
2022-12-29 12:04:01 +0100

    added timeaout for alert

--- front/src/Components/Alert.tsx
+++ front/src/Components/Alert.tsx
@@ -1,12 +1,15 @@
 import { IconAlertCircle, IconCheck, IconX } from "@tabler/icons";
-import { AlertType, removeAlert } from "../Redux/AlertSlice";
+import { useEffect, useState } from "react";
+import { AlertState, AlertType, removeAlert } from "../Redux/AlertSlice";
 import { useAppDispatch, useAppSelector } from "../Redux/Hooks";
 
-function Alert() {
-    const {alerts} = useAppSelector(state => state.alerts);
+function AlertItem(props: {elem: AlertState}) {
+    const {elem} = props;
+    const [timeoutId, setTimeoutId] = useState<ReturnType<typeof setTimeout> | undefined>(undefined);
     const dispatch = useAppDispatch();
 
     const closeSuccess = (id: number) => {
+        clearTimeout(timeoutId);
         dispatch(removeAlert(id));
     }
 
@@ -18,21 +21,40 @@ function Alert() {
         return "warning";
     }
 
+    useEffect(() => {
+        setTimeoutId(setTimeout(() => {
+            dispatch(removeAlert(elem.id));
+        }, 5000));
+
+        return () => {
+            clearTimeout(timeoutId);
+        }
+    }, [])
+
+    return (
+
+        <div key={elem.id} className={`notif-wrapper notif-${getClass(elem.type)}`}>
+            <div className="notif-content-container">
+                <div className="icon-wrapper">
+                    { elem.type === AlertType.SUCCESS && <IconCheck /> }
+                    { elem.type === AlertType.ERROR && <IconX /> }
+                    { elem.type === AlertType.WARNING && <IconAlertCircle /> }
+                </div>
+                <p> {elem.message} </p>
+                <IconX className="close-notif" onClick={() => closeSuccess(elem.id)} />
+            </div>
+        </div>
+    );
+}
+
+function Alert() {
+    const {alerts} = useAppSelector(state => state.alerts);
+
     return alerts.length > 0 ? (
         <div className="notif-container">
             {
                 alerts.map(elem => 
-                    <div key={elem.id} className={`notif-wrapper notif-${getClass(elem.type)}`}>
-                        <div className="notif-content-container">
-                            <div className="icon-wrapper">
-                                { elem.type === AlertType.SUCCESS && <IconCheck /> }
-                                { elem.type === AlertType.ERROR && <IconX /> }
-                                { elem.type === AlertType.WARNING && <IconAlertCircle /> }
-                            </div>
-                            <p> {elem.message} </p>
-                            <IconX className="close-notif" onClick={() => closeSuccess(elem.id)} />
-                        </div>
-                    </div>
+                    <AlertItem key={elem.id} elem={elem} />
                 )
             }
         </div>
