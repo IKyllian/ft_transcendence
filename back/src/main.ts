4d2eb9352e228cf0308a0fe25092497de665afb3
hikki <johan.chevet@hotmail.fr>
2022-12-30 13:16:48 +0100

    cleaned main

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,13 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { join } from 'path';
-import { Repository } from 'typeorm';
 import { AppModule } from './app.module';
-import { ChannelService } from './chat/channel/channel.service';
-import { ChannelUser, Channel, User, Statistic } from './typeorm';
-import { UserService } from './user/user.service';
-import { channelOption } from './utils/types/types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
