36c28a4aead0505458479ccad0c6a81fb118b32e
hikki <johan.chevet@hotmail.fr>
2023-01-03 15:02:21 +0100

    fixed type checking on guard

--- back/src/chat/channel/guards/ws-in-channel.guard.ts
+++ back/src/chat/channel/guards/ws-in-channel.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { isNumber } from "class-validator";
 import { NotInChannelException } from "src/utils/exceptions";
 import { ChannelService } from "../channel.service";
 
@@ -9,6 +10,11 @@ export class WsInChannelGuard implements CanActivate {
 		const ws_context = context.switchToWs();
 		const data = ws_context.getData();
 		const user = ws_context.getClient().user;
+		
+		if (!isNumber(data.chanId) || data.chanId < 0 || data.chanId > 2147483647) {
+			return false;
+		}
+
 		const channelUser = await this.channelService.getChannelUser(data.chanId, user.id);
 		if (!channelUser) {
 			throw new NotInChannelException();
